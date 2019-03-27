0fcda5e6b4625563d892e05e349680d59d1568c6
Cody Gillette <cgillette1312@gmail.com>
2019-03-27 08:31:08 -0700

    new

--- object.js
+++ object.js
@@ -15,3 +15,17 @@ function updateObject(obj) {
     obj.bang = 'bang';
     return obj;
   }
+
+  function personMaker() {
+    var person = {
+      firstName: 'Paul',
+      lastName: 'Jones',
+      // replace `null` with a function that uses self reference to return
+      // full name
+      fullName: function(){
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
+    return person;
+
+  }
