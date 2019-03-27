80d4d03452781df7df2c693e990c6037321d1569
Cody Gillette <cgillette1312@gmail.com>
2019-03-27 09:20:08 -0700

    a

--- objectDrills.js
+++ objectDrills.js
@@ -10,7 +10,11 @@
 // console.log(loaf.water);
 // console.log(loaf.hydration());
 
-let obj={foo:1,bar:"b",fun:"c",quux:4,spam:"asd"};
-for (let i in obj){
-    console.log(i+':'+obj[i]);
+// let obj={foo:1,bar:"b",fun:"c",quux:4,spam:"asd"};
+// for (let i in obj){
+//     console.log(i+':'+obj[i]);
+// }
+let obj2 = {
+    meal : ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']
 }
+console.log(obj2.meal[3]);
