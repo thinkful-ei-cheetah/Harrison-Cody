893b24a200d8e425457f9749a40d54bfaebc090c
hhcgit <hhc50331994@gmail.com>
2019-03-27 09:16:50 -0700

    2

--- objectDrills.js
+++ objectDrills.js
@@ -1,11 +1,16 @@
-let loaf ={
+// let loaf ={
 
-};
-loaf.flour = 300;
-loaf.water = 210;
-loaf.hydration=function(){
-    return loaf.water/loaf.flour * 100;
-};
-console.log(loaf.flour);
-console.log(loaf.water);
-console.log(loaf.hydration());
+// };
+// loaf.flour = 300;
+// loaf.water = 210;
+// loaf.hydration=function(){
+//     return loaf.water/loaf.flour * 100;
+// };
+// console.log(loaf.flour);
+// console.log(loaf.water);
+// console.log(loaf.hydration());
+
+let obj={foo:1,bar:"b",fun:"c",quux:4,spam:"asd"};
+for (let i in obj){
+    console.log(i+':'+obj[i]);
+}
