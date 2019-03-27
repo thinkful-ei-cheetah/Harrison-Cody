cf5b2a1d7a46dc0794643597ada9be800ddcee0f
hhcgit <hhc50331994@gmail.com>
2019-03-27 08:28:10 -0700

    2

--- object.js
+++ object.js
@@ -1,10 +1,17 @@
-function createMyObject() {
-    return {
-    foo:bar,
-    answerToUniver:42,
-    'olly olly':'oxen free',
-    sayHello : function(){
-      return 'hello'
-     }
-    }
+// function createMyObject() {
+//     return {
+//     foo:bar,
+//     answerToUniver:42,
+//     'olly olly':'oxen free',
+//     sayHello : function(){
+//       return 'hello'
+//      }
+//     }
+//   }
+function updateObject(obj) {
+    obj.foo = 'foo';
+    obj.bar = 'bar';
+    obj.bizz = 'bizz';
+    obj.bang = 'bang';
+    return obj;
   }
