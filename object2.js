7edbe8e3c69bfd80cef3de5ee0a3dae26420c69a
hhcgit <hhc50331994@gmail.com>
2019-03-27 08:50:56 -0700

    2

--- object2.js
+++ object2.js
@@ -1,3 +1,12 @@
 function makeStudentsReport(data) {
     return data.map(objs =>`${objs.name}: ${objs.grade}`);
   }
+function enrollInSummerSchool(students) {
+    return students.map(student => {
+      return {
+        name: student.name,
+        status: 'In Summer school',
+        course: student.course,
+      };
+    });
+  }
