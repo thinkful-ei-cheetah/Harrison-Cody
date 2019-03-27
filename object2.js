1da7f097f791eba7c41c60578a5b8b8164c2d596
hhcgit <hhc50331994@gmail.com>
2019-03-27 09:02:45 -0700

    4

--- object2.js
+++ object2.js
@@ -13,3 +13,15 @@ function enrollInSummerSchool(students) {
   function findById(items, idNum) {
     return items.find(item => item.id === idNum)
   }
+  function validateKeys(object, expectedKeys) {
+
+    if (Object.keys(object).length !== expectedKeys.length) {
+      return false;
+    }
+    for (let i = 0; i < expectedKeys.length; i++) {
+      if (!Object.keys(object).find(key => key === expectedKeys[i])) {
+        return false;
+      }
+    }
+    return true;
+  }
